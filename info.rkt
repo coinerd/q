145ea89a51af5be029b3db2e5ff6ca98ba77a8b8
coinerd <coinerd@users.noreply.github.com>
2026-04-14 22:24:13 +0200

    chore: bump version to 0.8.7

--- info.rkt
+++ info.rkt
@@ -2,7 +2,7 @@
 
 (define collection "q")
 (define pkg-name "q")
-(define version "0.8.6")
+(define version "0.8.7")
 (define pkg-desc "A local-first, extensible coding agent runtime")
 
 (define deps '("base"))
