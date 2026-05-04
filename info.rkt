b580c9f76efc27681da889613b899a801115a620
coinerd <coinerd@users.noreply.github.com>
2026-05-04 16:25:37 +0000

    chore: v0.29.4 Runtime State Encapsulation release (#3476)

--- info.rkt
+++ info.rkt
@@ -5,7 +5,7 @@
 
 (define collection "q")
 (define pkg-name "q")
-(define version "0.29.3")
+(define version "0.29.4")
 (define pkg-desc "A local-first, extensible coding agent runtime")
 
 (define deps '("base"))
