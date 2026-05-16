dfda66daf891ee8fb0f1c7c13267050e737e1092
coinerd <coinerd@users.noreply.github.com>
2026-05-16 06:27:45 +0000

    chore: v0.45.8 version bump + changelog + docs sync (#4378)

--- info.rkt
+++ info.rkt
@@ -5,7 +5,7 @@
 
 (define collection "q")
 (define pkg-name "q")
-(define version "0.45.7")
+(define version "0.45.8")
 (define pkg-desc "A local-first, extensible coding agent runtime")
 
 (define deps '("base"))
