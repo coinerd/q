2adfa3f507c1b056a8f231c20254f4d7f478b2d2
coinerd <coinerd@users.noreply.github.com>
2026-05-27 21:58:35 +0000

    feat(v0.61.8 W4): version bump to 0.61.8, CHANGELOG (#5733)

--- docs/architecture/overview.md
+++ docs/architecture/overview.md
@@ -2,7 +2,7 @@
 
 ## Version
 
-v0.61.7
+v0.61.8
 
 ## Native TUI Stack
 
--- util/version.rkt
+++ util/version.rkt
@@ -11,4 +11,4 @@
 
 (provide (contract-out [q-version string?]))
 
-(define q-version "0.61.7")
+(define q-version "0.61.8")
