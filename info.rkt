a90f5928899260902b04bb7c638e3cd487c9af38
coinerd <coinerd@users.noreply.github.com>
2026-05-28 02:28:23 +0000

    feat(v0.66.0 W4): version bump 0.65.0 to 0.66.0 + CHANGELOG (#5793)

--- info.rkt
+++ info.rkt
@@ -5,7 +5,7 @@
 
 (define collection "q")
 (define pkg-name "q")
-(define version "0.65.0")
+(define version "0.66.0")
 (define pkg-desc "A local-first, extensible coding agent runtime")
 
 (define deps '("base" "gui-easy-lib"))
