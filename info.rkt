b2479991499a608a766210cddf30bc3ae9e54746
coinerd <coinerd@users.noreply.github.com>
2026-06-01 17:55:20 +0000

    fix(v0.78.0): sync info.rkt version to 0.77.10 (#6526)

--- info.rkt
+++ info.rkt
@@ -5,7 +5,7 @@
 
 (define collection "q")
 (define pkg-name "q")
-(define version "0.77.9")
+(define version "0.77.10")
 (define pkg-desc "A local-first, extensible coding agent runtime")
 
 (define deps '("base" "gui-easy-lib"))
