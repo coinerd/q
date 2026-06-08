18da78db28fc62ddc123415230c497c1dfed70d5
coinerd <coinerd@users.noreply.github.com>
2026-06-08 09:44:04 +0000

    release: v0.96.0 Turn Orchestrator Decomposition (#7369)
    F1 god-module + F5 long-function resolved.
    turn-orchestrator.rkt: 534->286 LOC, 42->24 imports.
    New: turn-context.rkt (279 LOC), extension-setup.rkt (51 LOC).

--- info.rkt
+++ info.rkt
@@ -5,7 +5,7 @@
 
 (define collection "q")
 (define pkg-name "q")
-(define version "0.95.21")
+(define version "0.96.0")
 (define pkg-desc "A local-first, extensible coding agent runtime")
 
 (define deps '("base" "gui-easy-lib"))
