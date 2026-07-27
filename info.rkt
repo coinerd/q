144eda9411cd0ede061e8e83a874b99ecec399d7
coinerd <coinerd@users.noreply.github.com>
2026-07-27 08:38:03 +0000

    bump version to 0.99.70

--- info.rkt
+++ info.rkt
@@ -5,7 +5,7 @@
 
 (define collection "q")
 (define pkg-name "q")
-(define version "0.99.69")
+(define version "0.99.70")
 (define pkg-desc "A local-first, extensible coding agent runtime")
 
 (define deps '("base" "gui-easy-lib"))
